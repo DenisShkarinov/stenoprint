c35af2236b5b20a5e9b66bf2e4948e2658a8b7e8
Denis <67820415+DenisShkarinov@users.noreply.github.com>
2024-09-05 14:53:39 +0300

    good morning

--- app/components/header/header.tsx
+++ app/components/header/header.tsx
@@ -1,12 +1,17 @@
 import Logo from "@/app/components/header/Logo";
 import Links from "@/app/components/footer/links";
 import './header.css'
+import Search from "@/app/components/header/search";
+import InfoComponent from "@/app/components/footer/infoComponent";
 export default function Header () {
    return (
       <header >
           <Logo></Logo>
           <div className="menu">
-
+              <div className="top-menu">
+                  <Search></Search>
+                  <InfoComponent></InfoComponent>
+              </div>
               <nav>
                   <button className="btn ">Каталог товаров</button>
                   <Links></Links>
---
+++ app/components/header/search.tsx
@@ -0,0 +1,7 @@
+export default function Search () {
+    return (
+        <form>
+            <input type="search" placeholder="type here anything..."/>
+        </form>
+    )
+}
